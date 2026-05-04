bba7e40f7ad33282f28251b13c1015e39dd752de
Soyuz-dev <avon2ramesh@gmail.com>
2026-05-04 10:49:15 +0800

    morr feesix

--- src/main/kotlin/engine/physics/PhysicsBody.kt
+++ src/main/kotlin/engine/physics/PhysicsBody.kt
@@ -3,14 +3,11 @@ package org.soyuz.engine.physics
 import org.soyuz.util.Vector2D
 
 interface PhysicsBody {
-    var isStatic: Boolean
 
-    var mass: Float
+    var mass: Double
 
     var velocity: Vector2D
 
     fun applyForce(force: Vector2D)
-
-    fun setVelocity(x: Float, y: Float)
 }
 
