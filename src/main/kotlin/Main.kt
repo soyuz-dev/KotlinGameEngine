a9e38196962613476cf21229f10195e34bca769e
Soyuz-dev <avon2ramesh@gmail.com>
2026-05-08 12:02:40 +0800

    main has a demo now yay

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,30 @@
 package org.soyuz
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.opengl.GL
+import org.lwjgl.opengl.GL11.*
+import org.lwjgl.system.MemoryUtil.NULL
 
 fun main() {
-    println("hi :D")
-    readln()
-    println("halp :/")
-    Thread.sleep(100)
-    println("AAAAAAAAAAAAAAA")
+    // Initialize GLFW
+    if (!glfwInit()) throw IllegalStateException("Unable to initialize GLFW")
+
+    // Configure the window
+    val window = glfwCreateWindow(800, 600, "Kotlin LWJGL", NULL, NULL)
+    if (window == NULL) throw RuntimeException("Failed to create window")
+
+    glfwMakeContextCurrent(window)
+    GL.createCapabilities() // Connects LWJGL to the GPU's OpenGL context
+
+    // Main Loop
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+
+        // Render stuff here!
+
+        glfwSwapBuffers(window)
+        glfwPollEvents()
+    }
+
+    glfwDestroyWindow(window)
+    glfwTerminate()
 }
