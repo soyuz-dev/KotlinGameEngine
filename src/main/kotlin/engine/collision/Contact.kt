ea154820e1861b006352a32c16bd8dd4373533bd
Soyuz-dev <avon2ramesh@gmail.com>
2026-05-19 12:31:26 +0800

    contact position class for collision resolution

---
+++ src/main/kotlin/engine/collision/Contact.kt
@@ -0,0 +1,11 @@
+package org.soyuz.engine.collision
+
+import org.soyuz.util.Vector2D
+
+data class Contact(
+    val entityA: String,
+    val entityB: String,
+    val point: Vector2D,      // world-space contact point
+    val normal: Vector2D,     // collision normal, pointing from A toward B
+    val depth: Double         // penetration depth (positive = overlapping)
+)
