ced8f163ece2159eba0746bb598265dcf42907af
Soyuz-dev <avon2ramesh@gmail.com>
2026-05-19 10:22:53 +0800

    collisions can now happen

---
+++ src/main/kotlin/engine/collision/RuntimeCollisionSystem.kt
@@ -0,0 +1,35 @@
+package org.soyuz.engine.collision
+
+import org.soyuz.engine.scene.Scene
+import org.soyuz.util.Debug
+
+class RuntimeCollisionSystem : CollisionSystem {
+
+    private val colliders = mutableMapOf<String, Collider>()
+
+    override fun registerCollider(entityId: String, collider: Collider) {
+        colliders[entityId] = collider
+    }
+
+    override fun unregisterCollider(entityId: String) {
+        colliders.remove(entityId)
+    }
+
+    override fun detect(scene: Scene) {
+        val entities = scene.allEntities().filter { it.id in colliders }
+
+        for (i in entities.indices) {
+            for (j in i+1 until entities.size) {
+                val a = entities[i]
+                val b = entities[j]
+                val colliderA = colliders[a.id]!!
+                val colliderB = colliders[b.id]!!
+
+                if (colliderA.intersects(colliderB, a.transform, b.transform)) {
+                    //TODO: Produce Contact, fire events
+                    Debug.log {"Collision: ${a.id} hit ${b.id}"}
+                }
+            }
+        }
+    }
+}
