3464b277eb9bb0ec4e5885692d121563bde1258a
Soyuz-dev <avon2ramesh@gmail.com>
2026-05-20 08:23:25 +0800

    collision resolution step added to step loop

--- src/main/kotlin/engine/physics/RuntimePhysicsSystem.kt
+++ src/main/kotlin/engine/physics/RuntimePhysicsSystem.kt
@@ -1,9 +1,12 @@
 package org.soyuz.engine.physics
 
+import org.soyuz.engine.collision.CollisionSystem
 import org.soyuz.engine.scene.Scene
 import org.soyuz.util.Vector2D
 
-class RuntimePhysicsSystem : PhysicsSystem {
+class RuntimePhysicsSystem (
+    private val collisionSystem: CollisionSystem
+) : PhysicsSystem  {
 
     private val bodies = mutableMapOf<String, PhysicsBody>()
 
@@ -36,6 +39,48 @@ class RuntimePhysicsSystem : PhysicsSystem {
         }
 
         // Phase 2: Collision detection & resolution
+
+        val contacts = collisionSystem.detect(scene)
+
+        for (contact in contacts) {
+            val bodyA = bodies[contact.entityA]
+            val bodyB = bodies[contact.entityB]
+
+            val entityA = scene.findEntity(contact.entityA)?: continue
+            val entityB = scene.findEntity(contact.entityB)?: continue
+            if (bodyA == null || bodyB == null) continue
+
+            val invMassA = if (bodyA is RigidBody) bodyA.inverseMass else if (bodyA is PointMass) bodyA.inverseMass else 0.0
+            val invMassB = if (bodyB is RigidBody) bodyB.inverseMass else if (bodyB is PointMass) bodyB.inverseMass else 0.0
+
+            if (invMassA == 0.0 && invMassB == 0.0) continue
+
+            val relativeV = bodyB.velocity - bodyA.velocity
+            val vAlongNormal = relativeV dot contact.normal
+
+            if (vAlongNormal > 0) continue
+
+            val e = minOf(
+                bodyA.restitution,
+                bodyB.restitution
+            )
+
+            val j = -(1.0+e) * vAlongNormal / (invMassA + invMassB)
+            val impulse = contact.normal * j
+            bodyA.applyImpulse(-impulse, contact.point - entityA.transform.position)
+            bodyB.applyImpulse(impulse, contact.point - entityB.transform.position)
+
+            // Positional correction
+            val slop = 0.01
+            val percent = 0.8
+            val correctionMagnitude = maxOf(contact.depth - slop, 0.0) * percent / (invMassA + invMassB)
+            val correction = contact.normal * correctionMagnitude
+
+
+            entityA.transform = entityA.transform.translated(-correction * invMassA)
+            entityB.transform = entityB.transform.translated(correction * invMassB)
+
+        }
 
 
 
