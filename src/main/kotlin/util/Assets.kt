97dbf49c61ff705cf0e62fde17e5b073cda7340a
Soyuz-dev <avon2ramesh@gmail.com>
2026-06-12 15:54:22 +0800

    flaunting some Assets.kt

---
+++ src/main/kotlin/util/Assets.kt
@@ -0,0 +1,36 @@
+package org.soyuz.util
+
+import org.soyuz.engine.render.Shader
+import org.soyuz.engine.render.image.Texture
+
+object Assets {
+    private val shaders = mutableMapOf<String, Shader>()
+    private val textures = mutableMapOf<String, Texture>()
+    //private val audio = mutableMapOf<String, AudioClip>()
+
+    fun shader(name: String): Shader {
+        return shaders.getOrPut(name) {
+            Shader.fromResource("/shaders/$name.vert", "/shaders/$name.frag")
+        }
+    }
+
+    fun texture(name: String): Texture {
+        return textures.getOrPut(name) {
+            Texture.fromResource("/textures/$name.png")
+        }
+    }
+
+//    fun audio(name: String): AudioClip {
+//
+//        return audio.getOrPut(name) {
+//            AudioClip.fromResource("/audio/$name.wav")
+//        }
+//    }
+
+    fun cleanup() {
+        shaders.values.forEach { it.cleanup() }
+        textures.values.forEach { it.cleanup() }
+        //audio.values.forEach { it.cleanup() }
+        shaders.clear(); textures.clear(); //audio.clear()
+    }
+}
